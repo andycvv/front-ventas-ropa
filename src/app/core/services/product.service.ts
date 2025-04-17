717e9b43ab73eef2929ab1268db493c7a950b23f
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-17 11:19:52 -0500

    agregar provideHttpClient

---
+++ src/app/core/services/product.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:8080/product';
+
+  constructor(private http: HttpClient) { }
+}
