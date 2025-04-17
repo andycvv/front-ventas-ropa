26d3b786eeb3ba62265ed5a65b08e6ef23dd128e
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-17 18:55:18 -0500

    agregar routerLink al layout de admin

--- src/app/shared/layouts/admin-layout/admin-layout.component.ts
+++ src/app/shared/layouts/admin-layout/admin-layout.component.ts
@@ -1,4 +1,4 @@
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, DrawerModule, ButtonModule, CommonModule],
+  imports: [RouterOutlet, DrawerModule, ButtonModule, CommonModule, RouterLink],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
@@ -14,14 +14,14 @@ export class AdminLayoutComponent {
   collapsed = false;
 
   menuItems: any[] = [
-    { icon: 'pi pi-home', name: 'Panel', path: '/dashboard' },
-    { icon: 'pi pi-box', name: 'Productos', path: '/bookmarks' },
-    { icon: 'pi pi-tag', name: 'Categorías', path: '/reports' },
-    { icon: 'pi pi-palette', name: 'Colores', path: '/reports' },
-    { icon: 'pi pi-file', name: 'Tallas', path: '/reports' },
-    { icon: 'pi pi-map', name: 'Inventario', path: '/reports' },
-    { icon: 'pi pi-gift', name: 'Pedidos', path: '/reports' },
-    { icon: 'pi pi-users', name: 'Usuarios', path: '/reports' },
+    { icon: 'pi pi-home', name: 'Panel', path: '/admin' },
+    { icon: 'pi pi-box', name: 'Productos', path: 'productos' },
+    { icon: 'pi pi-tag', name: 'Categorías', path: 'categorias' },
+    { icon: 'pi pi-palette', name: 'Colores', path: 'colores' },
+    { icon: 'pi pi-file', name: 'Tallas', path: 'tallas' },
+    { icon: 'pi pi-map', name: 'Inventario', path: 'inventarios' },
+    { icon: 'pi pi-gift', name: 'Pedidos', path: 'pedidos' },
+    { icon: 'pi pi-users', name: 'Usuarios', path: 'usuarios' },
   ];
 
   user = {
