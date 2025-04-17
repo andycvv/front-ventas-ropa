2d1328d44d2da8bf71f65e82399292576c727321
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-17 11:41:54 -0500

    agregar ProductService

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../models/entities.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,16 @@ export class ProductService {
   private apiUrl = 'http://localhost:8080/product';
 
   constructor(private http: HttpClient) { }
+
+  getAll() {
+    return this.http.get<Product[]>(this.apiUrl);
+  }
+
+  getById(id: number) {
+    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+  }
+
+  save(product: Product) {
+    return this.http.post<Product>(this.apiUrl, product);
+  }
 }
