7b9cb1a417f4e77d8290a61b3f086ee133c69436
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-17 12:24:57 -0500

    agregar rutas de crear y editar a products.routes.ts

--- src/app/features/admin/product/products.routes.ts
+++ src/app/features/admin/product/products.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from "@angular/router";
 import { ProductListComponent } from "./pages/product-list/product-list.component";
+import { AddProductComponent } from "./pages/add-product/add-product.component";
+import { EditProductComponent } from "./pages/edit-product/edit-product.component";
 
 export const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: ProductListComponent
+  },
+  {
+    path: 'crear',
+    component: AddProductComponent
+  },
+  {
+    path: 'editar',
+    component: EditProductComponent
   }
 ]
