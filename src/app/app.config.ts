316c96c66a467c60bbc12611c05910bb6a976622
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-17 18:44:08 -0500

    agregar primeicons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,9 +16,7 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
-        options: {
-          darkModeSelector: '.my-app-dark'
-        }
+        options: { darkModeSelector: '.p-dark' }
       },
     })
   ]
