1c7a14fb916ccbf63dfeb1d9f982b25bf1377341
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-17 12:24:43 -0500

    agregar ruta de productos en admin.routes.ts

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from "@angular/router";
 
 export const routes: Routes = [
+  {
+    path: 'productos',
+    loadChildren: () => import('./product/products.routes').then(m => m.routes)
+  }
 ]
