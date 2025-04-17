d5c302d324e1dfbb62c92010241ac137299f2a80
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-17 12:25:11 -0500

    agregar router-outlet en los layouts

--- src/app/shared/layouts/client-layout/client-layout.component.ts
+++ src/app/shared/layouts/client-layout/client-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-client-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './client-layout.component.html',
   styleUrl: './client-layout.component.css'
 })
