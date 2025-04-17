49e366a8e2f0938c99dea0e98a203d456835a660
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-17 11:59:42 -0500

    agregar rutas en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { AdminLayoutComponent } from './shared/layouts/admin-layout/admin-layout.component';
+import { ClientLayoutComponent } from './shared/layouts/client-layout/client-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'client',
+    component: ClientLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./features/client/client.routes').then(m => m.routes)
+      }
+    ]
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import("./features/auth/auth.routes").then(m => m.routes)
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import("./features/admin/admin.routes").then(m => m.routes)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
