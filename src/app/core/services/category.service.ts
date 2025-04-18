af9ccd1db85db986ce5d922f5dd739bf89c9b332
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-18 12:08:33 -0500

    agregar CategoryService

---
+++ src/app/core/services/category.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Category } from '../models/entities.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private apiUrl = 'http://localhost:8080/category';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll() {
+    return this.http.get<Category[]>(this.apiUrl);
+  }
+
+  save(category: Category) {
+    return this.http.post<Category>(this.apiUrl, category);
+  }
+}
