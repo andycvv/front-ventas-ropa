ecda8b4fe7a8df88a82e13603691bead86ba521e
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-19 16:49:53 -0500

    agregar interfaces ImageProduct e Inventory

--- src/app/core/models/entities.interface.ts
+++ src/app/core/models/entities.interface.ts
@@ -4,25 +4,40 @@ export interface Product {
   description: string;
   price: number;
   createdAt?: string;
-  enabled: boolean;
+  enabled?: boolean;
   category: Category
 }
 
 export interface Category {
   id?: number;
   name?: string;
-  enabled: boolean;
+  enabled?: boolean;
 }
 
 export interface Color {
   id?: number;
   name?: string;
   code?: string;
-  enabled: boolean;
+  enabled?: boolean;
 }
 
 export interface Size {
   id?: number;
   value?: string;
-  enabled: boolean;
+  enabled?: boolean;
+}
+
+export interface ImageProduct {
+  id?: number;
+  url?: string;
+}
+
+export interface Inventory {
+  id?: number;
+  stock?: number;
+  enabled?: boolean;
+  product: Product;
+  size: Size;
+  color: Color;
+  imageProducts: ImageProduct[]
 }
