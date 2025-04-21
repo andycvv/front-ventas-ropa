83ba4844717a36c71ef7a97e5faf17a36454b44a
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-20 20:22:04 -0500

    agregar OrderService

---
+++ src/app/core/services/order.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Order } from '../models/entities.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  private apiUrl = 'http://localhost:8080/order'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll() {
+    return this.http.get<Order[]>(this.apiUrl)
+  }
+
+  save(order: Order) {
+    return this.http.post<void>(this.apiUrl, order)
+  }
+
+  getById(id: number) {
+    return this.http.get<Order>(this.apiUrl + `/${id}`)
+  }
+
+}
