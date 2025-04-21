0d0948b492dcb18c3e5592c12c630f2427515e94
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-20 20:22:22 -0500

    agregar ruta de order

---
+++ src/app/features/admin/order/orders.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { OrderListComponent } from "./pages/order-list/order-list.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: OrderListComponent
+  }
+]
