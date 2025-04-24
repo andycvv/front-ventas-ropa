d13071dcaab6b6efb087432d2ea950800b782719
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-24 08:19:34 -0500

    agregar rutas de producto para el cliente

---
+++ src/app/features/client/product/products.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+import { ProductListComponent } from "./pages/product-list/product-list.component";
+import { ProductDetailComponent } from "./pages/product-detail/product-detail.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductListComponent
+  },
+  {
+    path: 'producto/:id',
+    component: ProductDetailComponent
+  }
+]
