7559a1dbe580e6a4f9c7622f211fc2cc4a5a77e5
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-24 08:18:35 -0500

    agregar rutas del carrito

---
+++ src/app/features/client/cart/cart.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { CartListComponent } from "./pages/cart-list/cart-list.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: CartListComponent
+  }
+]
--- src/app/features/client/client.routes.ts
+++ src/app/features/client/client.routes.ts
@@ -1,5 +1,12 @@
 import { Routes } from "@angular/router";
 
 export const routes: Routes = [
-  
+  {
+    path: '',
+    loadChildren: () => import('./product/products.routes').then(m => m.routes)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.routes').then(m => m.routes)
+  }
 ]
