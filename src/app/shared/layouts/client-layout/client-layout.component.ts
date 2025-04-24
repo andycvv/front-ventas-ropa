93aa9fa52de44c6f61204a41543a350ffea9803b
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-24 08:19:20 -0500

    agregar layout del cliente ts

--- src/app/shared/layouts/client-layout/client-layout.component.ts
+++ src/app/shared/layouts/client-layout/client-layout.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-client-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './client-layout.component.html',
   styleUrl: './client-layout.component.css'
 })
 export class ClientLayoutComponent {
+  items: MenuItem[] | undefined;
+  userInitial = 'D'
+  showUserMenu = false
 
+  toggleUserMenu() {
+    this.showUserMenu = !this.showUserMenu;
+  }
+
+  logout() {
+
+  }
 }
