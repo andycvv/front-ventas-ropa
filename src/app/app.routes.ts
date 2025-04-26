f019c27e74bd26af54073e9f48b7442806def492
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-26 13:32:16 -0500

    agregar guards a las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { AdminLayoutComponent } from './shared/layouts/admin-layout/admin-layout.component';
 import { ClientLayoutComponent } from './shared/layouts/client-layout/client-layout.component';
+import { authGuard } from './core/guards/auth.guard';
+import { roleGuard } from './core/guards/role.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: ClientLayoutComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['CLIENTE'] },
     children: [
       {
         path: '',
@@ -15,11 +19,14 @@ export const routes: Routes = [
   },
   {
     path: 'auth',
+    component: ClientLayoutComponent,
     loadChildren: () => import("./features/auth/auth.routes").then(m => m.routes)
   },
   {
     path: 'admin',
     component: AdminLayoutComponent,
+    canActivate: [authGuard, roleGuard],
+    data: { roles: ['ADMIN'] },
     children: [
       {
         path: '',
