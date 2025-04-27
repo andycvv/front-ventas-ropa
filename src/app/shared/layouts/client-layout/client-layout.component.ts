1f6d53d8b73c79e3594dea4f0842f377e811329a
Andy Camilo Vargas Vargas <135892408+andycvv@users.noreply.github.com>
2025-04-26 20:45:17 -0500

    agregar numero de productos en el carrito al client-layout

--- src/app/shared/layouts/client-layout/client-layout.component.ts
+++ src/app/shared/layouts/client-layout/client-layout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { AuthService } from '../../../core/services/auth.service';
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-client-layout',
@@ -12,18 +13,21 @@ import { AuthService } from '../../../core/services/auth.service';
 export class ClientLayoutComponent implements OnInit {
   items: MenuItem[] | undefined;
   username = ''
-  showUserMenu = false
+  showUserMenu = false;
+  cartItemCount = 0;
 
   isLogged = false;
 
   constructor(
     private authService: AuthService,
+    private cartService: CartService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
     this.isLogged = this.authService.isLoggedIn()
     this.username = this.authService.getUsername() ?? ''
+    this.cartService.items.subscribe(items => this.cartItemCount = items.length)
   }
 
   toggleUserMenu() {
